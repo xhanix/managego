2b0f6a7ec255df743b617abffcb0cbd33e6ca035
Dr Hani Ebrahimi <hani@elucidaid.com>
2019-01-07 05:28:11 -0500

    * ManageGo.Android.csproj: Adding Notifications page
    * ManageGo.csproj:
    * ManageGo.iOS.csproj:
    * NotificationsPage.xaml:
    * NotificationsPageModel.cs:

---
+++ ManageGo/ViewModels/NotificationsPageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ManageGo
+{
+    internal class NotificationsPageModel : BaseDetailPage
+    {
+        internal override Task LoadData(bool refreshData = false, bool applyNewFilter = false)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
