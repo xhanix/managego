b899be8044f9cff0ef0832c38e19ba69ccac7f1e
Hani <xhanix@me.com>
2019-09-15 02:30:26 -0400

    Dev (#3)
    * pushed to store
    
    * fixed android issues

--- ManageGo/Models/BankTransaction.cs
+++ ManageGo/Models/BankTransaction.cs
@@ -80,7 +80,7 @@ namespace ManageGo.Models
         {
             get
             {
-                return Payments != null ? Payments.Count * 80 : 0;
+                return Payments != null ? Payments.Count * 85 : 0;
             }
         }
     }
